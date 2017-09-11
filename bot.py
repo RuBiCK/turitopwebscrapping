c310776dba6dfe2a565847b1e731493a974cfb8d
Rubén Fernández <rubick@gmail.com>
2017-09-11 22:21:48 +0200

    Added security for fixed chat ids and respond to two different services

--- bot.py
+++ bot.py
@@ -12,9 +12,10 @@ EMAIL = os.environ['EMAIL']
 PASSWORD = os.environ['PASSWORD']
 COMPANY = os.environ['COMPANY']
 TOKEN = os.environ['TOKEN']
+CHATSID = os.environ['CHATSIDS']
 
 loginUrl='https://app.turitop.com/admin/login/es/'
-urlData='https://app.turitop.com/admin/company/' + COMPANY + '/bookings/calendar'
+urlData = 'https://app.turitop.com/admin/company/' + COMPANY + '/calendar/monthly/' #Leter will add the service at the end of the URL
 
 loginData = {
     'user': EMAIL,
@@ -28,31 +29,41 @@ def telegramHandler(event, context):
 	handle(event['message'])
 
 def handle(msg):
-   bot = Bot(TOKEN)
-   # normal message
-   if 1 == 1:
-   #if flavor == 'normal':
-       content_type, chat_type, chat_id = telepot.glance(msg)
-       chat_id = msg['chat']['id']
-       #content_type, chat_type, chat_id = telepot.glance2(msg)
-       print('Normal Message:', content_type, chat_type, chat_id)
-       flavor = telepot.flavor(msg)
-       command = msg['text']
-       if command == '/start':
-           bot.sendMessage(chat_id, text='Hi! I am a Telegram Bot!!')
-       elif command == '/help':
-           bot.sendMessage(chat_id, text="I don't have any help commands yet!")
-       elif command == '/settings':
-           bot.sendMessage(chat_id, text="I cannot be configured via any settings yet. Check back soon!")
-       elif command == '/reservas':
-           bot.sendMessage(chat_id, text= "Dame unos segundos...")
-           logger.info('Llamando a webscrapping')
-           calendario = ws.weblogin(loginUrl,loginData,urlData)
-           logger.info('Post llamada')
-           bot.sendMessage(chat_id, text=calendario )
-       else:
-           bot.sendMessage(chat_id, text="No te entiendo. Por ahora solo entiendo '/reservas")
-       return('Message sent')
-
-   else:
-       raise telepot.BadFlavor(msg)
+    bot = Bot(TOKEN)
+    # normal message
+    if 1 == 1:
+    #if flavor == 'normal':
+		content_type, chat_type, chat_id = telepot.glance(msg)
+		chat_id = msg['chat']['id']
+        # only answer to a given list of authrized chats id
+		if chat_id in CHATSID:
+			#content_type, chat_type, chat_id = telepot.glance2(msg)
+			print('Normal Message:', content_type, chat_type, chat_id)
+			flavor = telepot.flavor(msg)
+			command = msg['text']
+			if command == '/start':
+				bot.sendMessage(chat_id, text='Hi! I am a Telegram Bot!!')
+			elif command == '/help':
+				bot.sendMessage(chat_id, text="I don't have any help commands yet!")
+			elif command == '/settings':
+				bot.sendMessage(chat_id, text="I cannot be configured via any settings yet. Check back soon!")
+			elif command == '/reservas Goya':
+				bot.sendMessage(chat_id, text="Dame unos segundos...")
+				logger.info('Empezando webscrapping')
+				calendario = ws.weblogin(loginUrl + 'P1',loginData,urlData)
+				logger.info('Finalizado Webscrapping')
+				bot.sendMessage(chat_id, text=calendario )
+			elif command == '/reservas Gotham':
+				bot.sendMessage(chat_id, text= "A ver a ver... cuantos grupitos para Gotham tenemos :)")
+				logger.info('Empezando webscrapping')
+				calendario = ws.weblogin(loginUrl + 'P2',loginData,urlData)
+				logger.info('Finalizado Webscrapping')
+				bot.sendMessage(chat_id, text=calendario )
+			else:
+				bot.sendMessage(chat_id, text="No te entiendo.")
+		else:
+			bot.sendMessage(chat_id, text="Lo siento, parece que no estas autorizado para usar mis fantasticos servicios ;)")
+			warningMessage='Attempt access from chatid: ' + chat_id
+			logger.warning(warnigMessage)
+		return('Message sent')
+		
